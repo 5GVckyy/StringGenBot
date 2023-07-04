b974760975dd9bd1d27095751b3f112eb46f48ff
5GVckyy <95718588+5GVckyy@users.noreply.github.com>
2023-07-04 22:10:39 +0700

    ```Update bot_users.py```

--- StringGenBot/bot_users.py
+++ StringGenBot/bot_users.py
@@ -1,16 +1,23 @@
 from pyrogram.types import Message
 from pyrogram import Client, filters
 
-from config import OWNER_ID
-from StringGenBot.db.users import add_served_user, get_served_users
+from env import OWNER_ID
+from StringGenBot.db import SESSION
+from StringGenBot.db.users_sql import Users, num_users
 
 
-@Client.on_message(filters.private & ~filters.service, group=1)
+@Client.on_message(~filters.service, group=1)
 async def users_sql(_, msg: Message):
-    await add_served_user(msg.from_user.id)
+    if msg.from_user:
+        q = SESSION.query(Users).get(int(msg.from_user.id))
+        if not q:
+            SESSION.add(Users(msg.from_user.id))
+            SESSION.commit()
+        else:
+            SESSION.close()
 
 
 @Client.on_message(filters.user(OWNER_ID) & filters.command("stats"))
 async def _stats(_, msg: Message):
-    users = len(await get_served_users())
-    await msg.reply_text(f"Statistik string saat ini :\n\n {users} User", quote=True)
+    users = await num_users()
+    await msg.reply(f"Statistik string saat ini :\n\n {users} User", quote=True)
