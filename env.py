dd1f40fd54de6fefe2f590c7df66937c401ee112
5GVckyy <95718588+5GVckyy@users.noreply.github.com>
2023-07-04 23:30:15 +0700

    Update env.py

--- env.py
+++ env.py
@@ -8,7 +8,7 @@ API_HASH = os.getenv("API_HASH", "").strip()
 BOT_TOKEN = os.getenv("BOT_TOKEN", "").strip()
 OWNER_ID = list(map(int, os.getenv("OWNER_ID", "").split()))
 DATABASE_URL = os.getenv("DATABASE_URL", "").strip()
-MUST_JOIN = os.getenv("MUST_JOIN", "")
+#  MUST_JOIN = os.getenv("MUST_JOIN", "")
 
 if not API_ID:
     print("No API_ID found. Exiting...")
